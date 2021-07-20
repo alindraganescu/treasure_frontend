05320c94ffdacbe6e81e66c3ef9d3e2c6c947df3
Alin Sergiu Draganescu <alindraganescu@gmail.com>
2021-07-20 11:23:50 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import { react } from '@babel/types';
+import axios from 'axios';
 
 function App() {
+  const [coinData, setCoinData] = useState();
+  const [newsData, setNewsData] = useState();
+  const [isLoading, setIsLoading] = useState();
+
+  //https://treasure-backend.herokuapp.com/home
+  // http://localhost:3000/home
+
+  useEffect(() => {
+    // setIsLoading(true);
+    axios.get('https://treasure-backend.herokuapp.com/home').then((res) => {
+      console.log(res.data);
+      // setCoinData(res.data.coinGecko);
+      // setNewsData(res.data.news);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -21,5 +40,7 @@ function App() {
     </div>
   );
 }
+
+//https://https://treasure-backend.herokuapp.com/
 
 export default App;
