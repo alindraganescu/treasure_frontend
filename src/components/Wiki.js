a1046d0d35e1b60e63dec93c533d8649259bd48b
Alin Sergiu Draganescu <alindraganescu@gmail.com>
2021-08-02 15:34:16 +0200

    style on wiki page

--- src/components/Wiki.js
+++ src/components/Wiki.js
@@ -6,146 +6,279 @@ const Wiki = () => {
     <div className="z-depth-2" id="wiki">
       <h5>RESOURCES LIST:</h5>
 
-      <table className="centered">
-        <tbody>
-          <tr>
-            <a
-              href="https://www.coingecko.com/en"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              CoinGecko
-            </a>
-          </tr>
-          <tr>
-            <a
-              href="https://www.binance.com/en"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Binance
-            </a>
-          </tr>
-          <tr>
-            <a
-              href="https://entethalliance.org/eea-members/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Ethereum Alliance
-            </a>
-          </tr>
-          <tr>
-            <a
-              href="https://cryptopanic.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Crypto Panic
-            </a>
-          </tr>
-          <tr>
-            <a
-              href="https://messari.io/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Messari
-            </a>
-          </tr>
-          <tr>
-            <a
-              href="https://www.reddit.com/r/CryptoCurrency/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Reddit - Cryptocurrency News & Discussion
-            </a>
-          </tr>
-          <tr>
-            <a
-              href="https://coinmarketcap.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              CoinMarketCap
-            </a>
-          </tr>
-          <tr>
-            <a
-              href="https://www.tradingview.com/markets/cryptocurrencies/prices-all/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              TradingView
-            </a>
-          </tr>
-          <tr>
-            <a
-              href="https://glassnode.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Glassnode
-            </a>
-          </tr>
-          <tr>
-            <a
-              href="https://cointrendz.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              CoinTrendz
-            </a>
-          </tr>
-          <tr>
-            <a
-              href="https://www.cryptocompare.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              CryptoCompare
-            </a>
-          </tr>
-          <tr>
-            <a
-              href="https://bitinfocharts.com/cryptocurrency-charts.html?__cf_chl_jschl_tk__=pmd_b7cfb114480903a3d190ec437e4ac7f98408019c-1627726091-0-gqNtZGzNAeKjcnBszQii"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              BitInfoCharts
-            </a>
-          </tr>
-          <tr>
-            <a
-              href="https://icodrops.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              IcoDrops
-            </a>
-          </tr>
-          <tr>
-            <a
-              href="https://coinlist.co/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Coinlist
-            </a>
-          </tr>
+      <div className="row">
+        <div className="col s6">
+          <h5>Your crypto resources:</h5>
+          <table className="centered" id="rows-wiki">
+            <thead>
+              <tr>
+                <th>Site</th>
+                <th>Description</th>
+                <th>Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  <a
+                    href="https://www.coingecko.com/en"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="small-shadow"
+                  >
+                    CoinGecko
+                  </a>
+                </td>
+                <td>Prices</td>
+                <td>
+                  <a className="material-icons tiny delete">delete_forever</a>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <a
+                    href="https://www.binance.com/en"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="small-shadow"
+                  >
+                    Binance
+                  </a>
+                </td>
+                <td>Exchange</td>
+                <td>
+                  <a className="material-icons tiny delete">delete_forever</a>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <a
+                    href="https://entethalliance.org/eea-members/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="small-shadow"
+                  >
+                    Ethereum Alliance
+                  </a>
+                </td>
+                <td>Organization</td>
+                <td>
+                  <a className="material-icons tiny delete">delete_forever</a>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <a
+                    href="https://cryptopanic.com/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="small-shadow"
+                  >
+                    Crypto Panic
+                  </a>
+                </td>
+                <td>News</td>
+                <td>
+                  <a className="material-icons tiny delete">delete_forever</a>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <a
+                    href="https://messari.io/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="small-shadow"
+                  >
+                    Messari
+                  </a>
+                </td>
+                <td>Prices</td>
+                <td>
+                  <a className="material-icons tiny delete">delete_forever</a>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <a
+                    href="https://www.reddit.com/r/CryptoCurrency/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="small-shadow"
+                  >
+                    Reddit - Cryptocurrency News & Discussion
+                  </a>
+                </td>
+                <td>Forum</td>
+                <td>
+                  <a className="material-icons tiny delete">delete_forever</a>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <a
+                    href="https://coinmarketcap.com/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="small-shadow"
+                  >
+                    CoinMarketCap
+                  </a>
+                </td>
+                <td>Prices</td>
+                <td>
+                  <a className="material-icons tiny delete">delete_forever</a>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <a
+                    href="https://www.tradingview.com/markets/cryptocurrencies/prices-all/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="small-shadow"
+                  >
+                    TradingView
+                  </a>
+                </td>
+                <td>Analyse</td>
+                <td>
+                  <a className="material-icons tiny delete">delete_forever</a>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <a
+                    href="https://glassnode.com/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="small-shadow"
+                  >
+                    Glassnode
+                  </a>
+                </td>
+                <td>Analyse</td>
+                <td>
+                  <a className="material-icons tiny delete">delete_forever</a>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <a
+                    href="https://cointrendz.com/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="small-shadow"
+                  >
+                    CoinTrendz
+                  </a>
+                </td>
+                <td>Alerts</td>
+                <td>
+                  <a className="material-icons tiny delete">delete_forever</a>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <a
+                    href="https://www.cryptocompare.com/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="small-shadow"
+                  >
+                    CryptoCompare
+                  </a>
+                </td>
+                <td>Analyse</td>
+                <td>
+                  <a className="material-icons tiny delete">delete_forever</a>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <a
+                    href="https://bitinfocharts.com/cryptocurrency-charts.html?__cf_chl_jschl_tk__=pmd_b7cfb114480903a3d190ec437e4ac7f98408019c-1627726091-0-gqNtZGzNAeKjcnBszQii"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="small-shadow"
+                  >
+                    BitInfoCharts
+                  </a>
+                </td>
+                <td>Analyse</td>
+                <td>
+                  <a className="material-icons tiny delete">delete_forever</a>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <a
+                    href="https://icodrops.com/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="small-shadow"
+                  >
+                    IcoDrops
+                  </a>
+                </td>
+                <td>ICOs</td>
+                <td>
+                  <a className="material-icons tiny delete">delete_forever</a>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <a
+                    href="https://coinlist.co/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="small-shadow"
+                  >
+                    Coinlist
+                  </a>
+                </td>
+                <td>ICOs</td>
+                <td>
+                  <a className="material-icons tiny delete">delete_forever</a>
+                </td>
+              </tr>
 
-          <tr>
-            <a
-              href="https://defirate.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              DeFi Rate
-            </a>
-          </tr>
-        </tbody>
-      </table>
+              <tr>
+                <td>
+                  <a
+                    href="https://defirate.com/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="small-shadow"
+                  >
+                    DeFi Rate
+                  </a>
+                </td>
+                <td>DeFi</td>
+                <td>
+                  <a className="material-icons tiny delete">delete_forever</a>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+        <div className="col s6">
+          <h5>Insert a favourite link:</h5>
+          <input
+            placeholder="Link address"
+            id="first_name"
+            type="text"
+            class="validate"
+          />
+          <button
+            type="sumbit"
+            className="btn-floating btn-large waves-effect waves-light red"
+          >
+            <i className="material-icons">add</i>
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
